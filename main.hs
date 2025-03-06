ec1017ff0284198edca829978e3afe847fc2f79e
Martin_Takacs <martin.takacs@dell.com>
2025-03-06 12:03:04 +0100

    logs

--- main.hs
+++ main.hs
@@ -7,22 +7,20 @@ main = do
 
 
     -- Obtain data from file to predict 
+    print "Loading data points"
     content <- readFile "test/data/test2.txt"
     let lines = splitLines ',' $ splitOn '\n' content
     let newdata = convertData lines
     print newdata
 
     -- Obtain tree from file
+    print "Loading tree"
     content_tree <- readFile "test/trees/treetest2.txt"
     let lines_tree = splitOn '\n' content_tree
     let tree = loadTree lines_tree
     print tree
 
-
-    -- Create test tree
-    -- let tree_test = Node 0 5.5 (Leaf "TridaA") (Node 1 3.0 (Leaf "TridaB") (Leaf "TridaC"))
-    -- print tree_test
-
     -- Predict (decide)
+    print "Calculating predictions"
     let predictions = predictTree tree newdata
     print predictions
