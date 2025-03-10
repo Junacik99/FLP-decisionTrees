3c8f06742200f1793b41f6202b5e2aa59229bd3f
Junacik99 <junacik99@gmail.com>
2025-03-10 14:23:35 +0100

    gini impurity

--- Training.hs
+++ Training.hs
@@ -6,19 +6,25 @@ import Data.List (sort, nub, elemIndex)
 -- Obtain column of features with index i
 getColumn i = map (!! i)
 
--- Get average values for adjacent points
+-- Get average values for adjacent points in one column
 getIntermediateValues [] = []
 getIntermediateValues [x] = []
 getIntermediateValues (x:y:xs) = (x + y) / 2.0 : getIntermediateValues (y:xs)
 
 initCounter classes = replicate (length classes) 0
 
+-- Get number of targets under threshold
 getUnderThreshold targets classes = init $ tail $ scanl updateCounter (initCounter classes) targets
     where updateCounter counter target =
             case elemIndex target classes of
                 Just idx -> take idx counter ++ [counter !! idx + 1] ++ drop (idx + 1) counter
                 Nothing  -> counter  -- If the target is not in the classes list, leave the counter unchanged
 
+-- Calculate Gini impurity
+giniImpurity targets = 1.0 - sum (map (\x -> (x / sum targets) ^ 2) targets)
+
+-- Calculate weighted average of Gini impurity
+weightedGiniImpurity under over = (sum under / (sum under + sum over)) * giniImpurity under + (sum over / (sum under + sum over)) * giniImpurity over
 
 -- Load train data + train tree
 training :: IO ()
@@ -28,11 +34,11 @@ training = do
 
     -- Load data
     content <- readFile "test/data/data_table.csv"
-    let loaded_data = splitLines ',' $ lines content
-    let train_features = convertData $ map init loaded_data
-    let train_targets = map last loaded_data
-    let train_data = zip train_features train_targets
-    let unique_targets = sort $ nub train_targets
+    let loaded_data = splitLines ',' $ lines content            -- Raw data
+    let train_features = convertData $ map init loaded_data     -- Features
+    let train_targets = map last loaded_data                    -- Targets
+    let train_data = zip train_features train_targets           -- Data (features + targets)
+    let unique_targets = sort $ nub train_targets               -- List of unique targets
     -- print unique_targets
     -- print train_features
     -- print train_targets
@@ -43,13 +49,27 @@ training = do
     -- 1. sort column                                                       check
     -- 2. get average intermediate values (between values)                  check
     -- 3. calculate Gini impurity for each of them - more calculations      TODO
-        -- a. for both branches -> 1 - probability of all classes squared
-        -- b. total gini impurity -> weighted average from two branches
-        -- c. select threshold with the lowest gini impurity
+        -- a. for both branches -> 1 - probability of all classes squared   check
+        -- b. total gini impurity -> weighted average from two branches     check
+        -- c. select threshold with the lowest gini impurity                TODO
     -- 4. select column (feature) with the lowest gini impurity
 
-    print $ getUnderThreshold train_targets unique_targets
+    -- Get under and over threshold counts
+    let under_threshold = getUnderThreshold train_targets unique_targets
+    let over_threshold = reverse $ getUnderThreshold (reverse train_targets) unique_targets
 
+    -- Get intermediate values
+    let intermediate_values = getIntermediateValues $ getColumn 2 train_features
+
+    -- Calculate Gini impurity for each threshold
+    -- let gini_under = map giniImpurity under_threshold
+    -- let gini_over = map giniImpurity over_threshold
+    -- print gini_under
+    -- print gini_over
+
+    -- Calculate weighted Gini impurity for each threshold
+    let weighted_gini = zipWith weightedGiniImpurity under_threshold over_threshold
+    print weighted_gini
 
     -- Create string from tree and delete last empty line
     -- let tree_to_save = init $ tree2string 0 tree_test
